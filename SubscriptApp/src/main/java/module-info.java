f0d83d8d0a6d39d6b6213a770d52bcf8d7bf5e17
Ciberdead <nico_goyon@orange.fr>
2023-12-02 15:26:03 +0100

    New project with bootstrapFX

--- SubscriptApp/src/main/java/module-info.java
+++ SubscriptApp/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.openjfx.javafxmavenarchetypes {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.kordamp.bootstrapfx.core;
 
     opens org.openjfx.javafxmavenarchetypes to javafx.fxml;
     exports org.openjfx.javafxmavenarchetypes;
